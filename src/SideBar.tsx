e27f5515c3f9a9d84eea73fb717a935972a842b0
jhvlima <joao.h.lima@edu.ufes.br>
2025-07-19 01:26:30 -0300

    feat: sideBar done

--- src/SideBar.tsx
+++ src/SideBar.tsx
@@ -1,45 +1,45 @@
 /* 
-  <li><Link to="/perfil">Perfil</Link></li>   -> recarrga apenas o componente MeuPerfil
   <li><a href="/perfil">Perfil</a></li>       -> recarrega toda a página
+  <li><Link to="/perfil">Perfil</Link></li>   -> recarrga apenas o componente MeuPerfil
 */
-
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { UserCircle, House, PlusCircle, DotsThreeOutlineVertical , SignOut } from 'phosphor-react';
 
 export function SideBar() {
   return (
+    <div className="sidebar items-center justify-center bg-white text-black p-4 w-60 h-screen border-r-4 border-black">
+      <img src="assets/logo_preta.png" alt="Logo" />
 
-<div className="sidebar  bg-white text-black p-4 w-60 h-screen border-r-4 border-orange-500">
-      <img src="assets/Logo.png" alt="Logo" />
-      
-      <ul className="space-y-4">
+      <ul className="space-y-4 items-center justify-center">
         <li>
-          <Link to="/perfil" className="flex items-center gap-2">
-            <img src="assets/account_circle.png" alt="Ícone de Perfil" className="w-5 h-5" />
+          <Link to="/perfil" className="flex items-center gap-3 p-3 rounded-lg transition-colors">
+            <UserCircle />
             <span>Perfil</span>
           </Link>
         </li>
         <li>
-          <Link to="/feed" className="flex items-center gap-2 ">
-            <img src="assets/Home.png" alt="Ícone de Feed" className="w-5 h-5" />
+          <Link
+            to="/feed" className="flex items-center gap-3 p-3 rounded-lg transition-colors">
+            <House />
             <span>Feed</span>
           </Link>
         </li>
         <li>
-          <Link to="/postar" className="flex items-center gap-2">
-            <img src="assets/icon.png" alt="Ícone de Postar" className="w-5 h-5" />
+          <Link to="/postar" className="flex items-center gap-3 p-3 rounded-lg transition-colors">
+            <PlusCircle />
             <span>Postar</span>
           </Link>
         </li>
         <li>
-          <Link to="/editar" className="flex items-center gap-2">
-            <img src="assets/more_vert.png" alt="Ícone de Editar" className="w-5 h-5" />
+          <Link to="/editar" className="flex items-center gap-3 p-3 rounded-lg transition-colors">
+            <DotsThreeOutlineVertical />
             <span>Editar</span>
           </Link>
         </li>
         <li>
-          <Link to="/sair" className="flex items-center gap-2">
-            <img src="assets/keyboard_return.png" alt="Ícone de Sair" className="w-5 h-5" />
+          <Link to="/sair" className="flex items-center gap-3 p-3 rounded-lg transition-colors">
+            <SignOut />
             <span>Sair</span>
           </Link>
         </li>
